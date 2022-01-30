0d91e56431ccb3971528bf4804d7d1f0e5a4aede
asheed <asheed@LAPTOP-AOFGI5CH>
2022-01-30 13:43:38 +0530

    sum of numbers

---
+++ javaintro/src/javaintro/sum.java
@@ -0,0 +1,17 @@
+package javaintro;
+
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+				System.out.println("enter a number....:");
+				int num1=scan.nextInt();
+				System.out.println("enter a another number....:");
+			    int num2=scan.nextInt();
+			    int sum=num1+num2;
+			    System.out.println("the sum of two numbers given is...:"+sum);
+		
+	}
+
+}
